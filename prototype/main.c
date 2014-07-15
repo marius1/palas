0d068ea02cfebb1dd89fc83ab6dde58c74a3cdc3
Marius <marius@bronstring.net>
2014-07-15 21:30:41 +0200

    empty project

---
+++ prototype/main.c
@@ -0,0 +1,14 @@
+#include "global.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <avr/io.h>		// include I/O definitions (port names, pin names, etc)
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	return 0;
+}
