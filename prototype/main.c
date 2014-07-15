a9b8739a0969c6f8bdbe6be29b52b73a29ad2c57
Marius <marius@bronstring.net>
2014-07-16 00:40:30 +0200

    print . on magnet detection

--- prototype/main.c
+++ prototype/main.c
@@ -8,7 +8,30 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#include "uart/uart.h"
+
+volatile uint16_t count = 0;
+volatile uint16_t rpm = 0;
+
 int main(void)
 {
+	uart_init();
+	DDRD &= ~(1 << DDD2);
+	EICRA |= (1 << ISC01);  // The falling edge of INT0 generates an interrupt request.
+	EIMSK |= (1 << INT0);	// Turn on INT0
+	
+	sei();
+	
+	uart_puts_p(PSTR("Init done!\n"));
+	
+	while(1)
+	{
+		
+	}
 	return 0;
+}
+
+ISR(INT0_vect)
+{
+	uart_putc('.');
 }
